803d3d6597af317a9ff477d4f7ba0806b171dff6
jijjoing <clue6288@gmail.com>
2021-12-06 17:43:58 +0900

    modify

--- myfirst_r_code.R
+++ myfirst_r_code.R
@@ -35,16 +35,12 @@ RColorBrewer::brewer.pal.info
 
 
 
-# 1. 참여유형별 노인 일자리 성별에 따른 참여자 수
-# = 할머니, 할아버지들 중에서 노인일자리를 참여하는 수가 어떻게 되는가? / 지속적으로 그 일을 하시는가?
-(
-    old_new %>% group_by(sex,participation) %>% 
-        summarise(participation_count = n()) %>% 
-        print() %>% 
-        ggplot(aes(x = sex, y = participation_count, fill=participation)) + geom_bar(stat ='identity') +
-        ggtitle("2020년 참여유형별 노인일자리사업 성별에 따른 참여자 수") +
-        geom_text(aes(label=participation_count),position = position_stack(vjust=0.5)) +
-        xlab("성별") + ylab("참여자 수")
-) %>% ggplotly()
+old_new %>% group_by(sex,participation) %>% 
+            summarise(participation_count = n()) %>% 
+            print() %>% 
+            ggplot(aes(x = sex, y = participation_count, fill=participation)) + geom_bar(stat ='identity') +
+            ggtitle("2020년 참여유형별 노인일자리사업 성별에 따른 참여자 수") +
+            geom_text(aes(label=participation_count),position = position_stack(vjust=0.5)) +
+            xlab("성별") + ylab("참여자 수")
 
 
